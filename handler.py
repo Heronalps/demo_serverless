2b0c42e2775edae68a5eb55605314221cda06445
Bryce Boe <bbzbryce@gmail.com>
2017-12-01 11:50:00 -0800

    Create submissions and show submissions on root page

--- handler.py
+++ handler.py
@@ -48,7 +48,7 @@ def redirect(path):
 
 
 def root(event, context):
-    html = """<h3>Submissions</h3>
+    body = """<h3>Submissions</h3>
 
 <table class="table">
   <thead>
@@ -60,15 +60,26 @@ def root(event, context):
     </tr>
   </thead>
 
-  <tbody>
-  </tbody>
+  <tbody>{}</tbody>
 </table>
 
 <br>
 <a class="btn btn-primary" href="submissions/new">New Submission</a>
 <a class="btn btn-primary" href="communities/new">New Community</a>
 """
-    return response(html)
+
+    row = """<tr>
+  <td><a href="{url}">{title}</a></td>
+  <td>{url}</td>
+  <td>{community}</td>
+  <td><a class="btn btn-primary btn-xs" href="submissions/{id}">0 comments</a></td>
+</tr>
+"""  # NOQA
+
+    table = DYNAMODB.Table('submissions')
+    submissions = [row.format(**x) for x in sorted(
+        table.scan()['Items'], key=lambda x: -x['createdAt'])]
+    return response(body.format(''.join(submissions)))
 
 
 def community_create(event, context):
@@ -103,13 +114,64 @@ def community_new(event, context):
 
 <form action="communities" method="post">
   <div class="field">
-    <label>Name<br><input type="text" name="community[name]"></label>
+    <label for="community_name">Name</label><br>
+    <input class="form-control" type="text" name="community[name]" id="community_name" /></label>
   </div>
   <div class="actions">
-    <input type="submit" value="Create Community" class="btn btn-primary">
+    <input type="submit" name="commit" value="Create Community" class="btn btn-primary" />
   </div>
 </form>
 
 <a href="communities">Back</a>
-"""
+"""  # NOQA
     return response(body)
+
+
+def submission_new(event, context):
+    body = """<h1>New Submission</h1>
+
+<form action="submissions" method="post">
+  <div class="field">
+    <label for="submission_title">Title</label><br>
+    <input class="form-control" type="text" name="submission[title]" id="submission_title" />
+  </div>
+  <div class="field">
+    <label for="submission_url">Url</label><br>
+    <input class="form-control" type="text" name="submission[url]" id="submission_url" />
+  </div>
+  <div class="field">
+    <label for="submission_community_id">Community</label><br>
+    <select class="form-control" name="submission[community_id]" id="submission_community_id">{}</select>
+  </div>
+  <div class="actions">
+    <input type="submit" name="commit" value="Create Submission" class="btn btn-primary" />
+  </div>
+</form>
+"""  # NOQA
+    table = DYNAMODB.Table('communities')
+    communities = sorted(x['title'] for x in table.scan()['Items'])
+    options = ''.join(['<option value="{0}">{0}</option>'
+                       .format(x) for x in communities])
+    return response(body.format(options))
+
+
+def submission_create(event, context):
+    data = parse_qs(event['body'])
+    community = data['submission[community_id]'][0]
+    title = data['submission[title]'][0]
+    url = data['submission[url]'][0]
+
+    table = DYNAMODB.Table('submissions')
+    now = int(time.time() * 1000)
+    item = {'community': community, 'createdAt': now, 'id': str(uuid1()),
+            'title': title, 'url': url}
+
+    try:
+        table.put_item(ConditionExpression='attribute_not_exists(id)',
+                       Item=item)
+    except botocore.exceptions.ClientError as exception:
+        code = exception.response['Error']['Code']
+        if code != 'ConditionalCheckFailedException':
+            raise
+        return response('{} already exists'.format(title))
+    return redirect('.')
